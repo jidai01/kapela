43eab359d61e23706c83db561bfa661febb7d338
jidai01 <lopesdacruzjeray@gmail.com>
2025-05-26 20:02:08 +0800

    Perbaiki error Middleware:CheckRole2

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -4,13 +4,14 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckRole
 {
     public function handle(Request $request, Closure $next, string $role): Response
     {
-        if (auth()->Guard::check() && auth()->Guard::user()->role === $role) {
+        if (Auth::check() && Auth::user()->role === $role) {
             return $next($request);
         }
 
