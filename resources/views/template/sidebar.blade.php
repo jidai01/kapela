9b419b4ba69f84d93041698ab7f4c4596585ef9c
jidai01 <lopesdacruzjeray@gmail.com>
2025-05-31 12:06:28 +0800

    Tambah sidebar

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -1,99 +1,99 @@
-<nav class="navbar navbar-expand-lg bg-secondary d-flex flex-column flex-md-column">
-    <div class="container-fluid border-bottom border-2 border-dark d-flex flex-column flex-md-column align-items-center justify-content-center p-3 px-4"
-        style="min-height: 80px;">
-        <img src="{{ asset('storage/img/logo.png') }}" alt="logo-sanbello" width="50" height="50"
-            class="border border-white rounded-circle mb-2 mb-md-0 md-5">
-        <h6 class="text-center m-0 responsive-heading"><strong>Kapela St. Agustinus Bello</strong></h6>
-    </div>
+<style>
+    #sidebarToggle {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        z-index: 1000;
+    }
 
-    <div class="container-fluid my-2">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav d-flex flex-column flex-md-column me-auto mb-2 mb-lg-0">
+    #sidebar {
+        padding-top: 60px;
+    }
+
+    .navbar-toggler {
+        display: inline-block;
+        cursor: pointer;
+    }
+
+    .bar1,
+    .bar2,
+    .bar3 {
+        width: 35px;
+        height: 5px;
+        background-color: black;
+        margin: 6px 0;
+        transition: 0.4s;
+    }
+
+    .bar1 {
+        transform: translate(0, 11px) rotate(-45deg);
+    }
+
+    .bar2 {
+        opacity: 0;
+    }
+
+    .bar3 {
+        transform: translate(0, -11px) rotate(45deg);
+    }
+
+    .change .bar1 {
+        transform: translate(0, -2px) rotate(0);
+    }
+
+    .change .bar2 {
+        opacity: 100;
+    }
+
+    .change .bar3 {
+        transform: translate(0, 2px) rotate(0);
+    }
+</style>
+
+<button class="navbar-toggler d-block border-0 rounded" type="button" id="sidebarToggle" aria-label="Toggle navigation"
+    onclick="myFunction(this)">
+    <div class="bar1"></div>
+    <div class="bar2"></div>
+    <div class="bar3"></div>
+</button>
+
+<nav class="navbar navbar-expand-lg bg-secondary d-flex flex-column min-vh-100" id="sidebar">
+    <div class="collapse show" id="sidebar-show">
+        <div class="container-fluid border-bottom border-2 border-dark d-flex flex-column align-items-center justify-content-center p-3 px-4"
+            style="min-height: 80px;">
+            <img src="{{ asset('storage/img/logo.png') }}" alt="logo-sanbello" width="50" height="50"
+                class="border border-white rounded-circle mb-2">
+            <h6 class="text-center m-0 responsive-heading"><strong>Kapela St. Agustinus Bello</strong></h6>
+        </div>
+
+        <div class="container-fluid my-2">
+            <ul class="navbar-nav d-flex flex-column w-100">
                 @if (Auth::check())
                     @php $role = Auth::user()->role; @endphp
                     <li class="nav-item">
                         <a class="nav-link py-1" href="/beranda/{{ $role }}">Beranda</a>
                     </li>
+
                     @if (in_array($role, ['admin', 'humas']))
-                        <li class="nav-item dropdown">
-                            <a class="nav-link py-1 dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">Data</a>
-                            <ul class="dropdown-menu p-0 py-1">
-                                @if ($role === 'admin')
-                                    <li>
-                                        <a class="dropdown-item d-flex justify-content-center"
-                                            href="/kelola/data-user">User</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item d-flex justify-content-center"
-                                            href="/kelola/data-wilayah">Wilayah</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item d-flex justify-content-center"
-                                            href="/kelola/data-kub">KUB</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item d-flex justify-content-center"
-                                            href="/kelola/data-sakramen">Sakramen</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item d-flex justify-content-center"
-                                            href="/kelola/data-umat">Umat</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item d-flex justify-content-center"
-                                            href="/kelola/data-kegiatan-wilayah">Kegiatan
-                                            Wilayah</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item d-flex justify-content-center"
-                                            href="/kelola/data-kegiatan-kub">Kegiatan
-                                            KUB</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item d-flex justify-content-center"
-                                            href="/kelola/data-penerimaan-sakramen">Penerimaan
-                                            Sakramen</a>
-                                    </li>
-                                @endif
-                                <li>
-                                    <a class="dropdown-item d-flex justify-content-center"
-                                        href="/kelola/data-pengumuman">Pengumuman</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item d-flex justify-content-center"
-                                        href="/kelola/data-berita">Berita</a>
-                                </li>
-                            </ul>
-                        </li>
+                        @if ($role === 'admin')
+                            <li><a class="nav-link" href="/kelola/data-user">Data User</a></li>
+                            <li><a class="nav-link" href="/kelola/data-wilayah">Data Wilayah</a></li>
+                            <li><a class="nav-link" href="/kelola/data-kub">Data KUB</a></li>
+                            <li><a class="nav-link" href="/kelola/data-sakramen">Data Sakramen</a></li>
+                            <li><a class="nav-link" href="/kelola/data-umat">Data Umat</a></li>
+                            <li><a class="nav-link" href="/kelola/data-kegiatan-wilayah">Data Kegiatan Wilayah</a></li>
+                            <li><a class="nav-link" href="/kelola/data-kegiatan-kub">Data Kegiatan KUB</a></li>
+                            <li><a class="nav-link" href="/kelola/data-penerimaan-sakramen">Data Penerimaan Sakramen</a>
+                            </li>
+                        @endif
+                        <li><a class="nav-link" href="/kelola/data-pengumuman">Data Pengumuman</a></li>
+                        <li><a class="nav-link" href="/kelola/data-berita">Data Berita</a></li>
                     @endif
 
                     @if (in_array($role, ['admin', 'ketua']))
-                        <li class="nav-item dropdown">
-                            <a class="nav-link py-1 dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">Laporan</a>
-                            <ul class="dropdown-menu p-0 py-1">
-                                <li>
-                                    <a class="dropdown-item d-flex justify-content-center"
-                                        href="/laporan/kegiatan-wilayah">Laporan
-                                        Kegiatan Wilayah</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item d-flex justify-content-center"
-                                        href="/laporan/kegiatan-kub">Laporan
-                                        Kegiatan KUB</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item d-flex justify-content-center"
-                                        href="/laporan/penerimaan-sakramen">Laporan
-                                        Penerimaan Sakramen</a>
-                                </li>
-                            </ul>
-                        </li>
+                        <li><a class="nav-link" href="/laporan/kegiatan-wilayah">Laporan Kegiatan Wilayah</a></li>
+                        <li><a class="nav-link" href="/laporan/kegiatan-kub">Laporan Kegiatan KUB</a></li>
+                        <li><a class="nav-link" href="/laporan/penerimaan-sakramen">Laporan Penerimaan Sakramen</a></li>
                     @endif
 
                     <li class="nav-item">
@@ -104,3 +104,18 @@
         </div>
     </div>
 </nav>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const toggleButton = document.getElementById("sidebarToggle");
+        const sidebarCollapse = document.getElementById("sidebar-show");
+
+        toggleButton.addEventListener("click", function() {
+            sidebarCollapse.classList.toggle("show");
+        });
+    });
+
+    function myFunction(x) {
+        x.classList.toggle("change");
+    }
+</script>
