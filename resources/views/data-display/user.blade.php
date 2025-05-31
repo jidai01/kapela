abd7ce30c3e42b2c8331823f5e19ed109ff6936a
jidai01 <lopesdacruzjeray@gmail.com>
2025-05-31 14:16:43 +0800

    View User

--- resources/views/data-display/user.blade.php
+++ resources/views/data-display/user.blade.php
@@ -4,43 +4,46 @@
     $hideFooter = true;
 @endphp
 
-@section('menu-navbar')
-    @include('template/navbar')
+@section('menu-sidebar')
+    @include('template/sidebar')
 @endsection
 
 @section('content')
-    <h2>Daftar User</h2>
-    <a href="/tambah-user" class="btn btn-sm btn-success m-2">Tambah User</a>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama User</th>
-                <th>Email</th>
-                <th>Peran</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($user as $index => $row)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $row->name }}</td>
-                    <td>{{ $row->email }}</td>
-                    <td>{{ $row->role }}</td>
-                    <td>
-                        <a href="/edit-user/{{ $row->id }}" class="btn btn-sm btn-warning">Edit</a> |
-                        <a href="/delete-user/{{ $row->id }}" class="btn btn-sm btn-danger"
-                            onclick="return confirm('Yakin ingin menghapus?')">Hapus</a>
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="7" class="text-center">Tidak ada data User.</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
-@endsection
+    <div class="container-fluid d-flex flex-column">
+        <h2 class="text-center">Daftar {{ $title }}</h2>
+        <a href="/tambah-user" class="btn btn-sm btn-success m-2 align-self-start">Tambah User</a>
 
-@include('template/bottom')
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead class="table-light">
+                    <tr>
+                        <th>No</th>
+                        <th>Nama User</th>
+                        <th>Email</th>
+                        <th>Peran</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($user as $index => $row)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $row->name }}</td>
+                            <td>{{ $row->email }}</td>
+                            <td>{{ $row->role }}</td>
+                            <td>
+                                <a href="/edit-user/{{ $row->id }}" class="btn btn-sm btn-warning">Edit</a>
+                                <a href="/delete-user/{{ $row->id }}" class="btn btn-sm btn-danger"
+                                    onclick="return confirm('Yakin ingin menghapus?')">Hapus</a>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5" class="text-center">Tidak ada data User.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
