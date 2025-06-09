b71739ed15b40891834f3024138c41b4ff87ec88
jidai01 <lopesdacruzjeray@gmail.com>
2025-06-10 07:03:17 +0800

    Buat Laporan sementara

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\KegiatanKub;
 use App\Models\KegiatanWilayah;
+use App\Models\PenerimaanSakramen;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class LaporanController extends Controller
@@ -11,27 +13,42 @@ class LaporanController extends Controller
     public function kegiatanwilayah()
     {
         $title = "Laporan Kegiatan Wilayah";
-        return view('report-admin.laporankegiatanwilayah', compact('title'));
+        return view('report-display/laporankegiatanwilayah', compact('title'));
+    }
+    
+    public function kegiatankub()
+    {
+        $title = "Laporan Kegiatan Kub";
+        return view('report-display/laporankegiatankub', compact('title'));
+    }
+    
+    public function penerimaansakramen()
+    {
+        $title = "Laporan Penerimaan Sakramen";
+        return view('report-display/laporanpenerimaansakramen', compact('title'));
     }
 
     public function cetakkegiatanwilayah(Request $request)
     {
-        $request->validate([
-            'bulan' => 'required|numeric|min:1|max:12',
-            'tahun' => 'required|numeric|min:2000',
-        ]);
+        $id_wilayah = $request->id_wilayah;
+        $kegiatanwilayah = KegiatanWilayah::where('id_wilayah', $id_wilayah)->get();
+        $pdf = Pdf::loadview('laporankegiatanwilayahpdf', compact('kegiatanwilayah'));
+        return $pdf->download();
+    }
 
-        $bulan = $request->bulan;
-        $tahun = $request->tahun;
+    public function cetakkegiatankub(Request $request)
+    {
+        $id_kub = $request->id_kub;
+        $kegiatankub = KegiatanKub::where('id_kub', $id_kub)->get();
+        $pdf = Pdf::loadview('laporankegiatankubpdf', compact('kegiatankub'));
+        return $pdf->download();
+    }
 
-        $kegiatan = KegiatanWilayah::with('wilayah')
-            ->whereMonth('tanggal_kegiatan', $bulan)
-            ->whereYear('tanggal_kegiatan', $tahun)
-            ->get();
-
-        $title = "Laporan Kegiatan Wilayah Bulan $bulan Tahun $tahun";
-
-        $pdf = Pdf::loadView('report-admin.laporankegiatanwilayah_pdf', compact('kegiatan', 'title', 'bulan', 'tahun'));
-        return $pdf->download("laporan-kegiatan-$bulan-$tahun.pdf");
+    public function cetakpenerimaansakramen(Request $request)
+    {
+        $id = $request->id;
+        $penerimaansakramen = PenerimaanSakramen::where('id', $id)->get();
+        $pdf = Pdf::loadview('laporanpenerimaansakramenpdf', compact('penerimaansakramen'));
+        return $pdf->download();
     }
 }
